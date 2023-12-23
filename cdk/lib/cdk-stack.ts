2ae9c6f7f8ea33afbaf895e63e9fdb7d5f07083b
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-12-23 18:38:44 +0100

    pass env to the lambda

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,10 +1,14 @@
 import * as path from 'node:path';
 
+import * as dotenv from 'dotenv';
+
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as nodeLambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+dotenv.config();
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,6 +21,15 @@ export class CdkStack extends cdk.Stack {
         runtime: lambda.Runtime.NODEJS_18_X,
         entry: path.join(__dirname, '../lambda/nest-server.ts'),
         handler: 'index.nestServer',
+
+        environment: {
+          NODE_ENV: process.env.NODE_ENV as string,
+          DB_HOST: process.env.DB_HOST as string,
+          DB_PORT: process.env.DB_PORT as string,
+          DB_USERNAME: process.env.DB_USERNAME as string,
+          DB_PASSWORD: process.env.DB_PASSWORD as string,
+          DB_NAME: process.env.DB_NAME as string,
+        },
       },
     );
 

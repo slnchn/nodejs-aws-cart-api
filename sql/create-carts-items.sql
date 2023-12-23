42b81b1ade888195ec707e0c6ba5f90aa810b575
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-12-23 14:03:16 +0100

    add sql for generating a database

---
+++ sql/create-carts-items.sql
@@ -0,0 +1,19 @@
+DROP TYPE IF EXISTS statuses;
+CREATE TYPE statuses AS ENUM ('OPEN', 'ORDERED');
+
+-- Create carts table
+CREATE TABLE carts (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL,
+    status statuses NOT NULL
+);
+
+-- Create cart_items table
+CREATE TABLE cart_items (
+    cart_id UUID REFERENCES carts(id),
+    product_id UUID,
+    count INTEGER,
+    PRIMARY KEY (cart_id, product_id)
+);

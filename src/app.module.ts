36d4185189504bd9e02a9fe777ac6e01be584393
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2023-12-23 15:47:41 +0100

    add postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 
 import { AppController } from './app.controller';
 
@@ -6,15 +8,29 @@ import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 
+dotenv.config();
+
 @Module({
   imports: [
     AuthModule,
     CartModule,
     OrderModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: false,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
   ],
-  controllers: [
-    AppController,
-  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { configure as serverlessExpress } from '@vendia/serverless-express';
 
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
+
+dotenv.config();
+
+// if dev run on localhost
+if (process.env.NODE_ENV !== 'production') {
+  (async () => {
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+      origin: (req, callback) => callback(null, true),
+    });
+
+    app.use(helmet());
+
+    await app.listen(4000);
+  })();
+}
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
